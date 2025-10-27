f1ad0bb252857a729684100269faf879f17754b1
cyw <101813508+traveller20000410@users.noreply.github.com>
2025-10-27 21:57:42 +0800

    Update transformer_GQA_Teacher.py

--- transformer_GQA_Teacher.py
+++ transformer_GQA_Teacher.py
@@ -17,7 +17,7 @@ def _fmha(q, k, v, p, bias, training: bool):
     )
     
 # 定义超参数，包括批量大小、训练轮次、学习率等
-BATCH_SIZE =        512;                        EPOCHS =        500
+BATCH_SIZE =        512;                        EPOCHS =        800
 LEARNING_RATE =     0.0003;                     D_MODEL =       64
 NUM_HEADS =         4;                          NUM_LAYERS =    4
 DROPOUT =           0.1;                        MAX_LENGTH =    1250
