aeca0011c30aa864e636c748ec81c8f9c6455d77
cyw <101813508+traveller20000410@users.noreply.github.com>
2025-11-04 09:54:09 +0800

    Update transformer_GQA_Student.py

--- transformer_GQA_Student.py
+++ transformer_GQA_Student.py
@@ -20,8 +20,8 @@ from torch.optim.lr_scheduler import StepLR, MultiStepLR, ReduceLROnPlateau, Cos
 # --- 学生模型的超参数 ---
 # 定义超参数，包括批量大小、训练轮次、学习率等
 BATCH_SIZE =        256;                        EPOCHS =        500   # 蒸馏可能需要更多/更少时间，取决于收敛情况
-LEARNING_RATE =     0.0002;                     D_MODEL =       32
-NUM_HEADS =         4;                          NUM_LAYERS =    4
+LEARNING_RATE =     0.0004;                     D_MODEL =       32
+NUM_HEADS =         4;                          NUM_LAYERS =    6
 DROPOUT =           0.1;                        MAX_LENGTH =    1250
 NUM_GROUPS =        2 ;                         PATIENCE=       20;   # 蒸馏训练可能需要更多耐心
 INITIAL_ALPHA =     0.5;                        FINAL_ALPHA =   0.1
